0f46945f3a5379ed5e02d7294e6db7ccc17d235a
CNevd <wangyalong20@gmail.com>
2016-05-18 11:47:29 +0800

    update run-yarn.sh

--- src/lbfgs-linear/run-yarn.sh
+++ src/lbfgs-linear/run-yarn.sh
@@ -12,9 +12,9 @@ hadoop fs -mkdir $2/data
 hadoop fs -put ../data/agaricus.txt.train $2/data
 
 # submit to hadoop
-../../repo/dmlc-core/tracker/dmlc_yarn.py  -n $1 --vcores 1  ./lbfgs.dmlc hdfs://$2/data/agaricus.txt.train model_out=hdfs://$2/mushroom.linear.model "${*:3}" 
+../../dmlc-core/tracker/dmlc_yarn.py  -n $1 --vcores 1  ./lbfgs.dmlc hdfs://$2/data/agaricus.txt.train model_out=hdfs://$2/mushroom.linear.model "${*:3}" 
 
 # get the final model file
 hadoop fs -get $2/mushroom.linear.model ./linear.model
 
-../../repo/dmlc-core/yarn/run_hdfs_prog.py ./lbfgs.dmlc ../data/agaricus.txt.test task=pred model_in=linear.model
+../../dmlc-core/yarn/run_hdfs_prog.py ./lbfgs.dmlc ../data/agaricus.txt.test task=pred model_in=linear.model
